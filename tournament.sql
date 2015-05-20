c2850d99350fe73ab02139271678a9b94d746b78
John Leszczynski <jrleszczynski@gmail.com>
2015-05-20 07:09:37 -0400

    add commands to create the initial database

--- tournament.sql
+++ tournament.sql
@@ -3,10 +3,10 @@
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
 --
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
-
+CREATE DATABASE tournament;
+-- connect to the newly created tournament database
+\c tournament;
 
 CREATE TABLE players (
     id serial PRIMARY KEY,
