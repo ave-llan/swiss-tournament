94fdbcc644966d6af7c6608b4043983016013709
John Leszczynski <jrleszczynski@gmail.com>
2015-05-20 11:18:39 -0400

    create Views

--- tournament.sql
+++ tournament.sql
@@ -21,3 +21,27 @@ CREATE TABLE matches (
     winner int REFERENCES players(id),
     loser int REFERENCES players(id)
 );
+
+--A view showing number of wins for each player
+CREATE VIEW win_record AS
+    SELECT players.id, count(matches.winner) as num
+    FROM players LEFT JOIN matches
+        on players.id = matches.winner
+    GROUP BY players.id
+    ORDER BY num DESC;
+
+--A view showing number of losses for each player
+CREATE VIEW loss_record AS
+    SELECT players.id, count(matches.loser) as num
+    FROM players LEFT JOIN matches
+        on players.id = matches.loser
+    GROUP BY players.id
+    ORDER BY num DESC;
+
+--A view of players id, name, wins, and games played sorted by number of wins
+CREATE VIEW standings AS
+    SELECT players.id, players.name, win_record.num AS wins, win_record.num + loss_record.num AS played
+    FROM players, win_record, loss_record
+    WHERE players.id = win_record.id AND players.id = loss_record.id
+    ORDER BY win_record.num DESC;
+
