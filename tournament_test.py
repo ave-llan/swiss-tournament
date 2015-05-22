7c04c29521fd97489528e273f84b4857a78925f4
John Leszczynski <jrleszczynski@gmail.com>
2015-05-22 17:18:52 -0400

    create new dog friendly unit tests

--- tournament_test.py
+++ tournament_test.py
@@ -1,139 +1,92 @@
-#!/usr/bin/env python
-#
-# Test cases for tournament.py
-
 from tournament import *
 
-def testDeleteMatches():
-    deleteMatches()
-    print "1. Old matches can be deleted."
+
+# register players
+dogs = ['Rudy', 'Jynx', 'Howie', 'Kip', 'August', 'Popeye']
+dogIDs = []
+for dog in dogs:
+    dogIDs.append((registerPlayer(dog), dog))
+
+# dogIDs
+#[(1, 'Rudy'), (2, 'Jynx'), (3, 'Howie'), (4, 'Kip'), (5, 'August'), (6, 'Popeye')]
 
 
-def testDelete():
-    deleteMatches()
-    deletePlayers()
-    print "2. Player records can be deleted."
+# register tour events
+events = ['Agility', 'Frisbee', 'Retrieval', 'Obedience', 'Tug of War']
+eventIDs = []
+for event in events:
+    eventIDs.append((registerTour(event), event))
+
+# eventIDs
+# [(1, 'Agility'), (2, 'Frisbee'), (3, 'Retrieval'), (4, 'Obedience'), (5, 'Tug of War')]
 
 
-def testCount():
-    deleteMatches()
-    deletePlayers()
-    c = countPlayers()
-    if c == '0':
-        raise TypeError(
-            "countPlayers() should return numeric zero, not string '0'.")
-    if c != 0:
-        raise ValueError("After deleting, countPlayers should return zero.")
-    print "3. After deleting, countPlayers() returns zero."
+# register all dogs for all tours
+for event in eventIDs:
+    for dog in dogIDs:
+        enrollPlayerInTour(dog[0], event[0])
+
+# test countPlayers(tour_id)
+for event in eventIDs:
+    if countPlayers(event[0]) != len(dogs):
+        raise ValueError("countPlayers(" + tour_id + ") incorrectly returned " + countPlayers(event[0]))
 
 
-def testRegister():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Chandra Nalaar")
-    c = countPlayers()
-    if c != 1:
-        raise ValueError(
-            "After one player registers, countPlayers() should be 1.")
-    print "4. After registering a player, countPlayers() returns 1."
+# test reportMatch
+
+# ROUND 1
+# Rudy draws with Jynx
+reportMatch(eventIDs[0][0], dogIDs[0][0], dogIDs[1][0], draw=True)
+# Howie beats Kip
+reportMatch(eventIDs[0][0], dogIDs[2][0], dogIDs[3][0])
+# August eats Popeye
+reportMatch(eventIDs[0][0], dogIDs[4][0], dogIDs[5][0])
 
 
-def testRegisterCountDelete():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Markov Chaney")
-    registerPlayer("Joe Malik")
-    registerPlayer("Mao Tsu-hsi")
-    registerPlayer("Atlanta Hope")
-    c = countPlayers()
-    if c != 4:
-        raise ValueError(
-            "After registering four players, countPlayers should be 4.")
-    deletePlayers()
-    c = countPlayers()
-    if c != 0:
-        raise ValueError("After deleting, countPlayers should return zero.")
-    print "5. Players can be registered and deleted."
+# ROUND 2
+# Howie beats August
+reportMatch(eventIDs[0][0], dogIDs[2][0], dogIDs[4][0])
+# Rudy beats Kip
+reportMatch(eventIDs[0][0], dogIDs[0][0], dogIDs[3][0])
+# Jynx eats Popeye
+reportMatch(eventIDs[0][0], dogIDs[1][0], dogIDs[5][0])
+
+# CHECKPOINT check Swiss Pairings
+expectedPairings = [dogIDs[2] + dogIDs[0], dogIDs[1] + dogIDs[4], dogIDs[3] + dogIDs[5]]
+actualPairings = swissPairings(eventIDs[0][0])
+
+for i in range(len(expectedPairings)):
+    if expectedPairings[i][0] != actualPairings[i][0] and expectedPairings[i][0] != actualPairings[i][2]:
+        raise ValueError('returned swissPairings do not match')
+    if expectedPairings[i][2] != actualPairings[i][0] and expectedPairings[i][2] != actualPairings[i][2]:
+        raise ValueError('returned swissPairings do not match')
+print 'swissPairings passed tests'
 
 
-def testStandingsBeforeMatches():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Melpomene Murray")
-    registerPlayer("Randy Schwartz")
-    standings = playerStandings()
-    if len(standings) < 2:
-        raise ValueError("Players should appear in playerStandings even before "
-                         "they have played any matches.")
-    elif len(standings) > 2:
-        raise ValueError("Only registered players should appear in standings.")
-    if len(standings[0]) != 4:
-        raise ValueError("Each playerStandings row should have four columns.")
-    [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)] = standings
-    if matches1 != 0 or matches2 != 0 or wins1 != 0 or wins2 != 0:
-        raise ValueError(
-            "Newly registered players should have no matches or wins.")
-    if set([name1, name2]) != set(["Melpomene Murray", "Randy Schwartz"]):
-        raise ValueError("Registered players' names should appear in standings, "
-                         "even if they have no matches played.")
-    print "6. Newly registered players appear in the standings with no matches."
+# ROUND 3
+# Rudy draws with Howie
+reportMatch(eventIDs[0][0], dogIDs[2][0], dogIDs[0][0], draw=True)
+# Jynx beats August
+reportMatch(eventIDs[0][0], dogIDs[1][0], dogIDs[4][0])
+# Kip eats Popeye
+reportMatch(eventIDs[0][0], dogIDs[3][0], dogIDs[5][0])
+
+# ROUND 4
+# Howie beats Jynx
+reportMatch(eventIDs[0][0], dogIDs[2][0], dogIDs[1][0])
+# Rudy beats Popeye
+reportMatch(eventIDs[0][0], dogIDs[0][0], dogIDs[5][0])
+# Kip beats August
+reportMatch(eventIDs[0][0], dogIDs[3][0], dogIDs[4][0])
 
 
-def testReportMatches():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Bruno Walton")
-    registerPlayer("Boots O'Neal")
-    registerPlayer("Cathy Burton")
-    registerPlayer("Diane Grant")
-    standings = playerStandings()
-    [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2)
-    reportMatch(id3, id4)
-    standings = playerStandings()
-    for (i, n, w, m) in standings:
-        if m != 1:
-            raise ValueError("Each player should have one match recorded.")
-        if i in (id1, id3) and w != 1:
-            raise ValueError("Each match winner should have one win recorded.")
-        elif i in (id2, id4) and w != 0:
-            raise ValueError("Each match loser should have zero wins recorded.")
-    print "7. After a match, players have updated standings."
+# CHECKPOINT check standings
 
-
-def testPairings():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Twilight Sparkle")
-    registerPlayer("Fluttershy")
-    registerPlayer("Applejack")
-    registerPlayer("Pinkie Pie")
-    standings = playerStandings()
-    [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2)
-    reportMatch(id3, id4)
-    pairings = swissPairings()
-    if len(pairings) != 2:
-        raise ValueError(
-            "For four players, swissPairings should return two pairs.")
-    [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4)] = pairings
-    correct_pairs = set([frozenset([id1, id3]), frozenset([id2, id4])])
-    actual_pairs = set([frozenset([pid1, pid2]), frozenset([pid3, pid4])])
-    if correct_pairs != actual_pairs:
-        raise ValueError(
-            "After one match, players with one win should be paired.")
-    print "8. After one match, players with one win are paired."
-
-
-if __name__ == '__main__':
-    testDeleteMatches()
-    testDelete()
-    testCount()
-    testRegister()
-    testRegisterCountDelete()
-    testStandingsBeforeMatches()
-    testReportMatches()
-    testPairings()
-    print "Success!  All tests pass!"
-
+expectedStandings = [(dogIDs[2][0], 3.5), (dogIDs[0][0], 3.0), (dogIDs[1][0], 2.5),
+                  (dogIDs[3][0], 2.0), (dogIDs[4][0], 1.0), (dogIDs[5][0], 0.0)]
+standings = playerStandings(eventIDs[0][0])
+for i in range(len(expectedStandings)):
+    if expectedStandings[i][0] != standings[i][0] or expectedStandings[i][1] != standings[i][6]:
+        raise ValueError('returned standings do not match')
+print 'playerStandings passed tests'
 
